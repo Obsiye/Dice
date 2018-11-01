d7f75a0a9a1820cd1c4208c620b6335940aee85f
Obsiye <mrabdirizak@gmail.com>
2018-11-01 12:16:55 +0000

    Refactoring and cleaning code

--- lib/dice.rb
+++ lib/dice.rb
@@ -3,10 +3,11 @@ class Dice
 
   def roll(n = 1)
     values = []
+
     n.times { values << rand(1..6)}
     values.each {|score| puts "The dice score is #{score}." }
+    
     values
   end
-
 
 end
--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -23,11 +23,9 @@ describe Dice do
   context 'User passes value into #roll' do
     it 'can #roll any number of dice at the same time' do
       expect(subject.roll(2).length).to eq 2
-      # expect(roll).to with(any_args)
     end  
     it 'can output the result of each dice #roll' do
-      expect(subject.roll)
-      expect{subject.roll(3)}.to output("The dice score is 1.\nThe dice score is 1.\nThe dice score is 5.\n").to_stdout
+      expect{subject.roll(3)}.to output("The dice score is 3.\nThe dice score is 1.\nThe dice score is 1.\n").to_stdout
     end
   end
 
