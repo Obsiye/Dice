a0aa071cf21a376c4d99460b99a04170f8cf852e
Obsiye <mrabdirizak@gmail.com>
2018-11-01 10:58:37 +0000

    Test #roll returns random number

--- spec/dice_spec.rb
+++ spec/dice_spec.rb
@@ -3,29 +3,29 @@ require 'dice'
 describe Dice do
  let (:dice) { Dice.new } 
 
+ before :each do
+  srand(2012)
+ end
+
   context 'User types #roll' do
     it 'can roll a dice' do
       expect(dice).to respond_to(:roll)
     end
 
     it 'returns number between 1 and 6' do
-      expect(dice.roll.pop).to be <= 6
-      expect(dice.roll.pop).to be >= 1
+      expect(dice.roll.pop).to be_between(1, 6)
     end
 
     it 'returns random number' do
-      expect(subject).to receive(:roll).and_return(4)
-      expect(subject.roll).to eq 4
+      expect(subject.roll.pop).to eq 3
     end
   end
 
   context 'User passes value into #roll' do
-
     it 'can #roll any number of dice at the same time' do
       expect(subject.roll(2).length).to eq 2
       # expect(roll).to with(any_args)
     end
-
   end
 
 end
